c2c0185837d8226c0932913deaf6fc57b09bf181
Willson Mock <willson.mock@gmail.com>
2015-01-12 22:54:06 -0500

    Complete anagram.js

--- willson/anagram.js
+++ willson/anagram.js
@@ -10,6 +10,29 @@
  * f('AbrAcadAbRa', 'cAda') // 2
 */
 
-var anagram = function (parent, child) {
+var numAnagrams = function (parent, child) {
+  var parentLen = parent.length;
+  var childLen  = child.length;
+  var result = 0;
+  var i, temp;
 
+  for (i = 0; i < parentLen - childLen; i += 1) {
+    temp = parent.slice(i, childLen + i);
+    if (isAnagram(temp, child)) {
+      result += 1;
+    }
+  }
+
+  return result;
+};
+
+var isAnagram = function (a, b) {
+  var aLen  = a.length;
+  var bLen  = b.length;
+  var first = b.indexOf(a[0]);
+
+  if (a === b)       { return true;  }
+  if (aLen !== bLen) { return false; }
+  if (first === -1)  { return false; }
+  return isAnagram( a.slice(1), b.slice(0, first) + b.slice(first + 1) );
 };
